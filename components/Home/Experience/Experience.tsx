c342780e95cdbc8fd1dce0b0cf526ed665a688fd
Your Name <alisapodgaynaya023@gmail.com>
2026-04-30 12:11:51 -0600

    UI: Experience section styles

--- components/Home/Experience/Experience.tsx
+++ components/Home/Experience/Experience.tsx
@@ -3,10 +3,12 @@ import styles from "./Experience.module.css";
 import { jobs, certifications } from "./experienceInfo";
 import { PiCalendarDotsThin } from "react-icons/pi";
 import { CiLocationOn } from "react-icons/ci";
+import { GoDotFill } from "react-icons/go";
 
 function Experience() {
   return (
-    <section id="experience" className={`${styles.experienceSection}`}>
+    <section id="experience" className={`${styles.experienceSection} section`}>
+      <div className={styles.glow}></div>
       <div className={`container ${styles.sectionWrapper}`}>
         <h2 className={styles.heading}>Experience</h2>
         <div className={styles.experienceCardsWrapper}>
@@ -14,16 +16,21 @@ function Experience() {
             <article key={job.id} className={styles.jobCard}>
               <div className={styles.topWrapper}>
                 <h3 className={styles.companyName}>{job.company}</h3>
-                <h4 className={styles.position}>{job.position}</h4>
+                <hr className={styles.divider} />
+                <h4>{job.position}</h4>
               </div>
 
               <ul className={styles.addInfo}>
                 <li className={styles.addInfoItem}>
-                  <Icon icon={PiCalendarDotsThin} size={24} />
+                  <Icon
+                    icon={PiCalendarDotsThin}
+                    size={24}
+                    className={styles.icon}
+                  />
                   {job.years}
                 </li>
                 <li className={styles.addInfoItem}>
-                  <Icon icon={CiLocationOn} size={24} />
+                  <Icon icon={CiLocationOn} size={24} className={styles.icon} />
                   {job.location}
                 </li>
               </ul>
@@ -31,6 +38,11 @@ function Experience() {
               <ul className={styles.responsibilities}>
                 {job.responsibilities.map((responsibility) => (
                   <li key={responsibility} className={styles.responsibility}>
+                    <Icon
+                      icon={GoDotFill}
+                      size={16}
+                      className={styles.iconBullet}
+                    />
                     {responsibility}
                   </li>
                 ))}
@@ -39,13 +51,24 @@ function Experience() {
           ))}
         </div>
 
-        <div className={styles.certsWrapper}>
-          <h3>Certifications</h3>
+        <div className={styles.certsificationsWrapper}>
+          <h3 className={styles.certHeading}>Certifications</h3>
           {certifications.map((certification) => (
-            <article key={certification.id}>
-              <h3 className={styles.certName}>{certification.name}</h3>
-              <h4 className={styles.certCompany}>{certification.company}</h4>
-              <p className={styles.certYear}>{certification.year}</p>
+            <article key={certification.id} className={styles.certWrapper}>
+              <ul className={styles.certList}>
+                <li>
+                  <p className={styles.certYear}>{certification.year}</p>
+                </li>
+
+                <li>
+                  <h4>{certification.name}</h4>
+                </li>
+                <li>
+                  <h4 className={styles.certCompany}>
+                    {certification.company}
+                  </h4>
+                </li>
+              </ul>
             </article>
           ))}
         </div>
