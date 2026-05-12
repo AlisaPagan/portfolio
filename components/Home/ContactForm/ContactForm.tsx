a458b7d1a78cfe40a776d3017feee4194dfaa6fe
Your Name <alisapodgaynaya023@gmail.com>
2026-05-11 21:21:54 -0600

    FEAT: add form validation, error messages UI: style form with error messages

--- components/Home/ContactForm/ContactForm.tsx
+++ components/Home/ContactForm/ContactForm.tsx
@@ -4,68 +4,108 @@ import Button from "@/components/UI/Button/Button";
 import styles from "./ContactForm.module.css";
 import Icon from "@/components/UI/Icon/Icon";
 import { VscSend } from "react-icons/vsc";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, FormikHelpers, ErrorMessage } from "formik";
 import { useId } from "react";
+import FormSchema from "./formValidation";
+
+interface FormValues {
+  name: string;
+  email: string;
+  subject?: string;
+  message: string;
+}
+
+const initialValues: FormValues = {
+  name: "",
+  email: "",
+  subject: "",
+  message: "",
+};
 
 function ContactForm() {
   const fieldId = useId();
 
+  const handleSubmit = (
+    values: FormValues,
+    actions: FormikHelpers<FormValues>,
+  ) => {
+    console.log("Form:", values);
+    actions.resetForm();
+  };
+
   return (
-    <Formik initialValues={{}} onSubmit={() => {}}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={FormSchema}
+    >
       <Form className={styles.contactForm}>
-        <label className={styles.inputLabel} htmlFor={`${fieldId}-name`}>
-          <span className={styles.inputLabelText}>
-            Your name <span className={styles.asterix}>*</span>
-          </span>
-        </label>
-        <Field
-          id={`${fieldId}-name`}
-          type="text"
-          name="name"
-          className={styles.input}
-          placeholder="John Doe"
-          required
-        />
+        <div className={styles.inputWrapper}>
+          <label className={styles.inputLabel} htmlFor={`${fieldId}-name`}>
+            <span className={styles.inputLabelText}>
+              Your name <span className={styles.asterix}>*</span>
+            </span>
+          </label>
+          <Field
+            id={`${fieldId}-name`}
+            type="text"
+            name="name"
+            className={styles.input}
+            placeholder="John Doe"
+          />
+          <p className={styles.error}>
+            <ErrorMessage name="name" />
+          </p>
+        </div>
 
-        <label className={styles.inputLabel} htmlFor={`${fieldId}-email`}>
-          <span className={styles.inputLabelText}>
-            Your email <span className={styles.asterix}>*</span>
-          </span>
-        </label>
-        <Field
-          id={`${fieldId}-email`}
-          type="email"
-          name="email"
-          className={styles.input}
-          placeholder="johndoe@example.com"
-          required
-        />
+        <div className={styles.inputWrapper}>
+          <label className={styles.inputLabel} htmlFor={`${fieldId}-email`}>
+            <span className={styles.inputLabelText}>
+              Your email <span className={styles.asterix}>*</span>
+            </span>
+          </label>
+          <Field
+            id={`${fieldId}-email`}
+            type="email"
+            name="email"
+            className={styles.input}
+            placeholder="johndoe@example.com"
+          />
+          <p className={styles.error}>
+            <ErrorMessage name="email" />
+          </p>
+        </div>
 
-        <label className={styles.inputLabel} htmlFor={`${fieldId}-subject`}>
-          <span className={styles.inputLabelText}>Subject</span>
-        </label>
-        <Field
-          id={`${fieldId}-subject`}
-          type="text"
-          name="subject"
-          className={styles.input}
-          placeholder="Portfolio / project inquiry"
-        />
+        <div className={styles.inputWrapper}>
+          <label className={styles.inputLabel} htmlFor={`${fieldId}-subject`}>
+            <span className={styles.inputLabelText}>Subject</span>
+          </label>
+          <Field
+            id={`${fieldId}-subject`}
+            type="text"
+            name="subject"
+            className={styles.input}
+            placeholder="Portfolio / project inquiry"
+          />
+        </div>
 
-        <label className={styles.inputLabel} htmlFor={`${fieldId}-message`}>
-          <span className={styles.inputLabelText}>
-            Your message <span className={styles.asterix}>*</span>
-          </span>
-        </label>
-        <Field
-          id={`${fieldId}-message`}
-          name="message"
-          className={styles.textArea}
-          placeholder="Hi Alisa, I'd like to talk about..."
-          required
-          minLength={100}
-          maxLength={800}
-        ></Field>
+        <div className={styles.inputWrapper}>
+          <label className={styles.inputLabel} htmlFor={`${fieldId}-message`}>
+            <span className={styles.inputLabelText}>
+              Your message <span className={styles.asterix}>*</span>
+            </span>
+          </label>
+          <Field
+            id={`${fieldId}-message`}
+            name="message"
+            className={styles.textArea}
+            placeholder="Hi Alisa, I'd like to talk about..."
+            as="textarea"
+          ></Field>
+          <p className={styles.error}>
+            <ErrorMessage name="message" />
+          </p>
+        </div>
 
         <Button type="submit" className={styles.formBtn}>
           Send message
---
+++ components/Home/ContactForm/formValidation.ts
@@ -0,0 +1,18 @@
+import * as Yup from "yup";
+
+const FormSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Name is too short. Use at least 2 characters.")
+    .max(40, "Name is too long. Use at least 40 characters.")
+    .required("Please input your name."),
+  email: Yup.string()
+    .email("Please enter a valid email.")
+    .required("Please input your email."),
+  subject: Yup.string(),
+  message: Yup.string()
+    .min(100, "Message is too short. Use at least 100 characters.")
+    .max(800, "Message is too long. Use at least 800 characters.")
+    .required("Please enter your message."),
+});
+
+export default FormSchema;
